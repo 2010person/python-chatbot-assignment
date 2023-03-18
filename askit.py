523b2eaf8ccfb1527240e34904dbe4db146bf8dd
2010person <116949561+2010person@users.noreply.github.com>
2023-03-18 21:27:43 +0000

    Pending changes exported from your codespace

--- askit.py
+++ askit.py
@@ -1,5 +1,6 @@
 from autocorrect import Speller
 from ScrapeSearchEngine.SearchEngine import Google
+from webpage_reader import analyse, read_page
 
 # The command below from the autocorrect module creates a function which can then be used to correct spelling from user-inputed strings
 spell = Speller()
@@ -10,4 +11,10 @@ chat = input("Enter:")
 gg = spell(chat)
 # The command below searches google 
 gg_searched = Google(gg, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36 Edg/112.0.0.02")
-print(gg_searched[1:1])
+
+# The block of code below analyzes the webpages
+url=gg_searched[1]
+headers = {}
+page_text = read_page(url=url, headers=headers)
+result = analyse(page_text=page_text, url=url)
+print(result)
