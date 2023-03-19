b68009e1151acb324e87e2f3ee25e13d5b8ff3f6
programmerthe <116949561+2010person@users.noreply.github.com>
2023-03-19 11:48:49 +0000

    Added askitt functionality

--- askit.py
+++ askit.py
@@ -1,13 +1,18 @@
-from autocorrect import Speller
-from ScrapeSearchEngine.SearchEngine import Google
-
-# The command below from the autocorrect module creates a function which can then be used to correct spelling from user-inputed strings
-spell = Speller()
+from ScrapeSearchEngine.ScrapeSearchEngine import Google
+import requests
+from bs4 import BeautifulSoup
 
 chat = "default startup text"
 
-chat = input("Enter:")
-gg = spell(chat)
-# The command below searches google 
-gg_searched = Google(gg, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36 Edg/112.0.0.02")
-print(gg_searched[1:1])
+def process_questions():
+    global chat, spell
+    chat = input("Enter:")
+    # The command below searches google
+    gg_searched = Google(chat, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0  x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36 Edg/112.0.0.0")
+    # Below we analyze the webpages that has been found on Google
+    PAGE = requests.get(gg_searched[0])
+    SOUP = BeautifulSoup(PAGE.text, 'html.parser')
+    for TAG in SOUP.find_all('p'):
+        if TAG.string is not None:
+            print(TAG.string)
+process_questions()
