7fb5f24971ce4b095412b749531cfc7c4fc4d76e
2010person <116949561+2010person@users.noreply.github.com>
2023-03-31 14:59:11 +0000

    Pending changes exported from your codespace

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Commits not welcome, sorry this is a school project
 # It is a chatbot-website, feel free to view 
 # But it isn't finished yet...
+# The .env file contains some GitHub Secrets
---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, render_template
+import openai
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/chatwithai", methods=["POST", "GET"])
+def chatwithai():
+
+    openai.api_key = "OPENAIAPIKEY"
+
+    prompt = request.form['prompt']
+    response = openai.Completion.create(
+        engine="davinci",
+        prompt=prompt,
+        max_tokens=2048,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+
+    message = {'user': prompt, 'ai': response.choices[0].text}
+
+    return render_template('index.html', message=message)
+
+if __name__ == "__main__":
+    app.run(debug=True)
