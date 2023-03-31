990a555f11a4c39662c698ccedd3a6069d4a34eb
2010person <116949561+2010person@users.noreply.github.com>
2023-03-31 15:46:41 +0000

    Pending changes exported from your codespace

--- askit.py
+++ askit.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 chat = "default startup text"
 
 def process_questions():
-    global chat, spell
+    global chat
     chat = input("Enter:")
     # The command below searches google
     gg_searched = Google(chat, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0  x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36 Edg/112.0.0.0")
@@ -15,4 +15,5 @@ def process_questions():
     for TAG in SOUP.find_all('p'):
         if TAG.string is not None:
             print(TAG.string)
+        
 process_questions()
